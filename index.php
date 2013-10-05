e091a066c4e368faabf4c0320fc543bbf0355d98
julia <rms.julle@gmail.com>
2013-10-05 01:53:07 -0500

    quitar conexion

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 
 <?php 
 
+	/*
 	include 'conexion.php';
 
 	require_once("src/facebook.php");
@@ -16,6 +17,7 @@
 		  'scope' => 'read_stream',
 		  'redirect_uri' => 'http://whowho.herokuapp.com/'
 		);
+		*/
 ?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
