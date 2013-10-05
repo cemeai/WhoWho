c3b1706cf842205b5b5786c737b1705c48c70094
unknown <cesar.amaro@20d.mx>
2013-10-05 02:06:40 -0500

    problema 3

--- session.php
+++ session.php
@@ -10,27 +10,3 @@ if(empty($_SESSION['uid'])) { // Recuerda usar corchetes.
 	header('Location: index.php');
 } // Recuerda usar corchetes
 ?>
-
-<!DOCTYPE html>
-<html lang="es">
-<head>
-<title>Creamos y mostramos la sesión</title>
-</head>
-<body>
-<div class="c1">
-<h2>Mostramos los datos guardados</h2>
-<section>
-<p>
-Tu nombre de usuario es <?=$_SESSION['uid'];?>
-</p>
-</section>
-</div>
- 
-<div class="c2">
-<section>
-<p>
-<a href="./index.html">Eliminar sesión</a> <!-- de esta forma se crea la nueva session, sin necesidad de crear otro script en php. -->
-</p>
-</section>
-</div>
-</body>
