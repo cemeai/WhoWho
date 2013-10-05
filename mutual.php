fd87c321632c1ea64abf2916395db504282aa80b
julia <rms.julle@gmail.com>
2013-10-05 02:38:57 -0500

    despues de cesar v5

--- mutual.php
+++ mutual.php
@@ -6,40 +6,41 @@
 							<p class="titulo tit_cotizacion">Mutual Friends with </p>
 						
 			
-							<div class="clear"></div>
+						<div class="clear"></div>
 
-								<?php 
-									/* Login request to Facebook */
-									$loginUrl = $facebook->getLoginUrl($params);
-			  						/* Link to login */
-			  						echo '<a href="'.$loginUrl.'">Facebook Login</a>';
+						<?php 
+							/* Login request to Facebook */
+							$loginUrl = $facebook->getLoginUrl($params);
+	  						/* Link to login */
+	  						echo '<a href="'.$loginUrl.'">Facebook Login</a>';
 
-			  						/* Logged in user id */
-			  						$uid = $facebook->getUser();
+	  						/* Logged in user id */
+	  						$uid = $facebook->getUser();
 
-			  						if($uid > 0){
-			  							/* Get 2 people mutual friends */
-				  						$path = $uid;
-				  						$method = 'GET';
-				  						$params = array('fields' => 'mutualfriends.user(653347944).fields(picture,name)');
-				  						$mutualFriendsResponse = $facebook->api($path, $method, $params);
+	  						if($uid > 0){
+	  							/* Get 2 people mutual friends */
+		  						$path = $uid;
+		  						$method = 'GET';
+		  						$params = array('fields' => 'mutualfriends.user(653347944).fields(picture,name)');
+		  						$mutualFriendsResponse = $facebook->api($path, $method, $params);
 
-				  						/* Get mutual friends of response */
-				  						$mutualFriends = $mutualFriendsResponse["mutualfriends"]["data"];
-				  						//var_dump($mutualfriends);
+		  						/* Get mutual friends of response */
+		  						$mutualFriends = $mutualFriendsResponse["mutualfriends"]["data"];
 
-				  						foreach ($mutualFriends as $mutualFriend) {
-				  							echo '</br>';
-				  							echo '<div id="'.$mutualFriend['id'].'" class="mutual_personal">'
-				  							echo '<h6>'.$mutualFriend['name'].'</h6>';
-				  							echo '<p> userId = '.$mutualFriend['id'].'</p>';
-				  							echo '<img src="'.$mutualFriend['picture']['data']['url'].'" />';
-				  							echo '</div>';
-				  							echo '</br>';
-				  						}
+		  						//var_dump($mutualfriends);
 
-			  						}
-								?>
+		  						foreach ($mutualFriends as $mutualFriend) {
+		  							echo '</br>';
+		  							echo '<div id="'.$mutualFriend['id'].'" class="mutual_personal">'
+		  							echo '<h6>'.$mutualFriend['name'].'</h6>';
+		  							echo '<p> userId = '.$mutualFriend['id'].'</p>';
+		  							echo '<img src="'.$mutualFriend['picture']['data']['url'].'" />';
+		  							echo '</div>';
+		  							echo '</br>';
+		  						}
+
+	  						}
+						?>
 						</div>
 						<div class="clear"></div>
 					</div>
