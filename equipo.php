67fbb36c5c7f0b2c97d1aaf5ff083e46af50cab7
julia <rms.julle@gmail.com>
2013-10-05 00:22:50 -0500

    replace todo

---
+++ equipo.php
@@ -0,0 +1,70 @@
+
+
+		<div class="infoContainer">
+			<p class="titulo">Are we ?</p>
+			
+			<div class="equipoCont">
+				<div class="personaCont" id="prs1">
+					<img src="../images/julia.png" class="img-polaroid" />
+					<p class="nombrePersona">Julia Ramos</p>
+					<p class="puestoPersona">B.S. Computer Science and Technology @Tec de Monterrey </p>
+					<a href="mailto:rms.julle@gmail.com"><p class="emailPersona">rms.julle&#64;gmail.com</p></a>
+				</div>
+
+				<div class="personaCont" id="prs2">
+					<img src="../images/karen.png" class="img-polaroid" />
+					<p class="nombrePersona">Karen Olvera </p>
+					<p class="puestoPersona">B.S. Computer Science and Technology @Tec de Monterrey </p>
+					<a href="mailto:kareneolvera@gmail.com"><p class="emailPersona">kareneolvera&#64;gmail.com</p></a>
+				</div>
+
+				<div class="personaCont" id="prs3">
+					<img src="../images/raul.png" class="img-polaroid" />
+					<p class="nombrePersona">Ra&uacute;l&nbsp;G&oacute;mez</p>
+					<p class="puestoPersona">B.S. Computer Science and Technology @Tec de Monterrey </p>
+					<a href="mailto:raul.gomez.mont@gmail.com"><p class="emailPersona">raul.gomez.mont&#64;gmail.com</p></a>
+				</div>
+				<div class="clear" id="clear_equipo" style="height: 16px;"></div>
+
+				<div class="personaCont" id="prs4">
+					<img src="../images/cesar.png" class="img-polaroid" />
+					<p class="nombrePersona">C&eacute;sar Amaro</p>
+					<p class="puestoPersona">B.S. Computer Science and Technology @Tec de Monterrey </p>
+					<a href="mailto:cesaral_92@hotmail.com"><p class="emailPersona">cesaral_92&#64;hotmail.com</p></a>
+				</div>
+
+				<div class="personaCont" id="prs5">
+					<img src="../images/osvaldo.png" class="img-polaroid" />
+					<p class="nombrePersona">Osvaldo Marines</p>
+					<p class="puestoPersona">Engineering of Informatics and Communication Technologies @Tec de Monterrey </p>
+					<a href="mailto:osvaldo.marines@gmail.com"><p class="emailPersona">osvaldo.marines&#64;gmail.com</p></a>
+				</div>
+
+
+			</div>
+
+
+			<div class="clear"></div>
+			
+		</div>
+
+		<!--<div class="lineaGris"></div>-->
+		<hr>
+
+
+		<div class="clear"></div>
+
+		<script type="text/javascript">
+	
+			$("document").ready(function() {
+
+				 $("div.personaCont").mouseenter(function() {
+				    $(this).find("img.img-polaroid").css("opacity","1.0");
+				  }).mouseleave(function() {
+				    $(this).find("img.img-polaroid").css("opacity","0.9");
+				  });
+
+
+			});
+	
+		</script>
